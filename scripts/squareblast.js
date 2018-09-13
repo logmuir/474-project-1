98fb66cbfe88e3c56941104e08c0ec7f30f3b39e
RJ Bowen <rjbowen97@gmail.com>
2018-09-13 10:20:43 -0400

    Formatted and removed remaining code

--- scripts/squareblast.js
+++ scripts/squareblast.js
@@ -1,28 +1,11 @@
-var futballGame=function(){
-    var self=this;
-    this.options={
-        height:500,
-        width:680,
-        goalHeight:188,
-        goalRight:50,
-        goalWidth:50,
-        minRange:600,
-        minX:50,
-        minY:100,
-        mindelay:1000,
-        maxdelay:5000,
-        minballSpeed:70,
-        maxballSpeed:150,
-        errorPercent:1.25,
-        goalieHeight:60
-    }
-    this.height=500;
+var futballGame = function () {
+    var self = this;
 
-    this.initialize=function(){
+    this.initialize = function () {
 
     };
 
-    this.update=function(){
+    this.update = function () {
 
         return 0;
 
--- scripts/squareblastUI.js
+++ scripts/squareblastUI.js
@@ -1,60 +1,51 @@
-var futballUI=function()
-{
-    var self=this;
-    this.game=undefined;
-    this.running=false;
-    this.initialize=function()
-    {
-        self.game=new futballGame()
+var futballUI = function () {
+    var self = this;
+    this.game = undefined;
+    this.running = false;
+    this.initialize = function () {
+        self.game = new futballGame()
 
-           $('#GameStopped').show();
-            $('#GameRunning').hide();
+        $('#GameStopped').show();
+        $('#GameRunning').hide();
 
-      $('body').keypress(function(event){
-            if (event.which==97) // Presses 'A' key
+        $('body').keypress(function (event) {
+            if (event.which == 97) // Presses 'A' key
             {
 
             }
             else if (event.which = 122) // Presses 'D' key
             {
-                
+
             }
         });
-        $('#StartBtn').on('click',function(){
+        $('#StartBtn').on('click', function () {
             $('#GameStopped').hide();
             $('#GameRunning').show();
             $('#Status').text('Get Ready...');
-            self.running=true;
+            self.running = true;
         });
-        $('#StopBtn').on('click',function(){
+        $('#StopBtn').on('click', function () {
             $('#GameStopped').show();
             $('#GameRunning').hide();
-            self.running=false;
-            self.game.reset();
+            self.running = false;
             self.refreshView();
         });
     };
-    this.refreshView=function(){
-        $('#futball').css("left",self.game.ball.xPos-7);
-        $('#futball').css("top",self.game.ball.yPos-7);
-        $('#goalie').css("top",self.game.goaltender.Position+'px');
-        $('#AttemptCount').text(self.game.shotsTaken);
-        $('#MissCount').text(self.game.shotsMissed);
+    this.refreshView = function () {
+
     };
 
-    this.updateUI=function()
-    {
-        if (self.running==false)
-        {
+    this.updateUI = function () {
+        if (self.running == false) {
             return;
         }
-            var result=self.game.update();
-            self.refreshView();
+        var result = self.game.update();
+        self.refreshView();
 
-            if (result==0){
-                setTimeout(function(){self.updateUI();},10);
-                return;
-            }
+        if (result == 0) {
+            setTimeout(function () { self.updateUI(); }, 10);
+            return;
+        }
     }
     this.initialize();
 }
