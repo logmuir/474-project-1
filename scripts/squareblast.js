dc497388cd5e00c3e0fcd1ea4cf7012a80c12dd0
RJ Bowen <rjbowen97@gmail.com>
2018-09-18 10:44:57 -0400

    Makes enemy squares correctly

--- scripts/squareblast.js
+++ scripts/squareblast.js
@@ -1,13 +1,17 @@
 var SquareBlastGame = function () {
     var self = this;
 
+    self.enemySquares = [];
+
     this.initialize = function () {
         var htmlToInsert = "";
         for (var currentIndex = 0; currentIndex < 10; currentIndex++) {
+            self.enemySquares.push(new enemySquare());
             htmlToInsert += "<div id='enemySquare" + currentIndex + "'></div>";
         }
 
         document.getElementById("randomSquaresTargetDiv").outerHTML = htmlToInsert;
+        console.log(self.enemySquares);
     };
 
     this.update = function () {
@@ -18,5 +22,13 @@ var SquareBlastGame = function () {
     };
 
     this.initialize();
+
+}
+
+var enemySquare = function () {
+    this.xPos = 0;
+    this.yPos = 0;
+
+    //direction
 
 }
