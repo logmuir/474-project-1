c39e1a57736ab06ed761a395cba421f0432d8032
RJ Bowen <rjbowen97@gmail.com>
2018-09-24 23:52:12 -0400

    Square boi is moving!

--- scripts/squareblast.js
+++ scripts/squareblast.js
@@ -1,9 +1,11 @@
 var SquareBlastGame = function () {
     var self = this;
-
+    self.player = undefined;
     self.enemySquares = [];
 
     this.initialize = function () {
+        self.player = new player();
+
         var htmlToInsert = "";
         for (var currentIndex = 0; currentIndex < 10; currentIndex++) {
             self.enemySquares.push(new enemySquare());
@@ -14,18 +16,47 @@ var SquareBlastGame = function () {
         console.log(self.enemySquares);
     };
 
-    this.update = function () {
+    this.updateView = function () {
 
-
+        // console.log('blorp');
+        self.player.updateSprite();
         return 0;
-
     };
 
     this.initialize();
 
 }
 
-var enemySquare = function () {
+var player = function() {
+    var self = this;
+    self.xPosition = 0;
+    self.yPosition = 0;
+    self.moveDx = 4;
+    self.moveDy = 4;
+    self.playerDiv = document.getElementById('player');
+
+    self.updateSprite = function () {
+        console.log(self.playerDiv);
+        self.playerDiv.style.left = self.xPosition + 'px';
+        self.playerDiv.style.top = self.yPosition + 'px';
+    }
+
+    self.movePlayerUp = function() {
+        self.yPosition += self.moveDx;
+    }
+    self.movePlayerLeft = function() {
+        self.xPosition -= self.moveDx;
+    }
+    self.movePlayerRight = function() {
+        self.xPosition += self.moveDx;
+    }
+    self.movePlayerDown = function() {
+        self.yPosition += self.moveDx;
+    }
+}
+
+
+var enemySquare =function () {
     this.xPos = 0;
     this.yPos = 0;
 
--- scripts/squareblastUI.js
+++ scripts/squareblastUI.js
@@ -1,23 +1,29 @@
 var SquareBlastUI = function () {
     var self = this;
-    this.game = undefined;
-    this.running = false;
-    this.initialize = function () {
+    self.game = undefined;
+    self.running = false;
+    self.initialize = function () {
         self.game = new SquareBlastGame()
 
         $('#GameStopped').show();
         $('#GameRunning').hide();
 
-        $('body').keypress(function (event) {
-            if (event.which == 97) // Presses 'A' key
-            {
-
+        document.addEventListener('keydown', function(event) {
+            console.log(self.game);
+            if (event.keyCode == 68) { //right
+                self.game.player.movePlayerUp();
             }
-            else if (event.which = 122) // Presses 'D' key
-            {
-
+            if (event.keyCode == 65) { //left
+                self.game.player.movePlayerLeft();
             }
-        });
+            if (event.keyCode == 87) { //up
+                self.game.player.movePlayerRight();
+            }
+            if (event.keyCode == 83) { //down
+                self.game.player.movePlayerDown();
+            }
+        })
+
         $('#StartBtn').on('click', function () {
             $('#GameStopped').hide();
             $('#GameRunning').show();
@@ -29,20 +35,15 @@ var SquareBlastUI = function () {
             $('#GameStopped').show();
             $('#GameRunning').hide();
             self.running = false;
-            self.refreshView();
         });
-    };
-    this.refreshView = function () {
-
     };
 
     this.updateUI = function () {
         if (self.running == false) {
             return;
         }
-        var result = self.game.update();
-        self.refreshView();
-
+        var result = self.game.updateView();
+        console.log(result);
         if (result == 0) {
             setTimeout(function () { self.updateUI(); }, 10);
             return;
